61fc7bf2fabb7016091478665601a0414b2f4161
Tushar Gupta <65303620+tgupta25@users.noreply.github.com>
2022-06-11 12:11:50 -0700

    Update CallonCallPricing.m

--- DerivativesPricing/CallonCallPricing.m
+++ DerivativesPricing/CallonCallPricing.m
@@ -1,6 +1,6 @@
-%% Pricing option on option
+%% Pricing option on option.
 
-%% Stock and other details
+%% Stock and other details.
 
 stock = 100;
 sigma = 0.1;
@@ -9,13 +9,13 @@ call_stk = 98;
 call_maturity = 2;
 calloncall_maturity = 1;
 
-%% black scholes price of parent call
+%% black scholes price of parent call.
 [parentcallprice notused] = blsprice(stock,call_stk,rate,...
     call_maturity,sigma);
 
 calloncall_stk = parentcallprice;
 
-%% simulate using gbm
+%% simulate using gbm.
 dynamics = gbm(rate, sigma ,'StartState', stock);
 steps = 360*2;
 nTrials = 20000;
@@ -23,14 +23,14 @@ DeltaTime = 1/360;
 s = simulate(dynamics, steps, 'nTrials',nTrials,'DeltaTime',DeltaTime);
 s = squeeze(s); %%removes dimensions of length 1 with the same elements.
 
-%% plot and see
+%% plot.
 plot(s);
 
-%% Parent Price at 1-year point
+%% Parent Price at 1-year point.
 
 parent_prices = blsprice(s(361,:), call_stk   ,rate,1,sigma);
 
-%% call on call pricing
+%% call on call pricing.
 
 calloncall_price = ...
 exp(-rate*calloncall_maturity)*...
